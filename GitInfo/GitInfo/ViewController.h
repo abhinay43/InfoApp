d16b611882cd335be1c58b39c2ce7fbf0b6b9d0e
Abhinay <abhinay.mca09@gmail.com>
2015-10-17 11:04:39 +0530

    First Commit: A

---
+++ GitInfo/GitInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitInfo
+//
+//  Created by Abhinay on 17/10/15.
+//  Copyright © 2015 ons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitInfo/GitInfo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitInfo
+//
+//  Created by Abhinay on 17/10/15.
+//  Copyright © 2015 ons. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
