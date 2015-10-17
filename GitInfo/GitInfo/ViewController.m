8929d0b3ef197a4035554d8fd919a9f6090fb538
Abhinay <abhinay.mca09@gmail.com>
2015-10-17 11:38:21 +0530

    fgt

--- GitInfo/GitInfo/ViewController.m
+++ GitInfo/GitInfo/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [UIDevice currentDevice].finalize;
+    [UIDevice currentDevice];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
