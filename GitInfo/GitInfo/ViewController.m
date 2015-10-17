80234f4f97eed09398e8e8cae9912223f782c647
Abhinay <abhinay.mca09@gmail.com>
2015-10-17 11:29:39 +0530

    mc

--- GitInfo/GitInfo/ViewController.m
+++ GitInfo/GitInfo/ViewController.m
@@ -14,8 +14,10 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    [UIDevice currentDevice];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
